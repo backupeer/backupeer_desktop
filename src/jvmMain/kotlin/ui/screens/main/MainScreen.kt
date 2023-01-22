d9a087d158dd3e2b72e7ee87692181c0c15de462
joaovictorjs <skai7ami@gmail.com>
2023-01-22 15:09:09 -0300

    rotas

--- src/jvmMain/kotlin/ui/screens/main/MainScreen.kt
+++ src/jvmMain/kotlin/ui/screens/main/MainScreen.kt
@@ -7,15 +7,30 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import di.MainScreenModule
+import org.kodein.di.instance
 import theme.ColorSchemaProvider
 import theme.Theme
+import ui.controller.MainScreenController
+import ui.controller.Route
+import ui.screens.analyzer.AnalyzerScreen
 
 @Composable
 fun MainScreen() {
+    val ctrl by MainScreenModule.instance<MainScreenController>()
+
     Row(
         modifier = Modifier.fillMaxSize().background(ColorSchemaProvider.current.surface),
         horizontalArrangement = Arrangement.Start
     ) {
         Sidebar()
+
+        when (ctrl.actualRoute) {
+            Route.ANALYZER -> {
+                AnalyzerScreen()
+            }
+
+            else -> {}
+        }
     }
 }
